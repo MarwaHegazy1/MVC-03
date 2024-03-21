7680af9e64f2d9b6a28e7d64b66eff40fabf8325
MarwaHegazy1 <marwahegazy599@gmail.com>
2024-03-21 23:12:37 +0200

    DepartmentController - Index

--- ASP.NET Core MVC.DAL/Modules/Department.cs
+++ ASP.NET Core MVC.DAL/Modules/Department.cs
@@ -16,6 +16,7 @@ namespace ASP.NET_Core_MVC.DAL.Modules
         public string Code { get; set; }
         //[Required]
         public string Name { get; set; }// Option => ASP.NET Core 5
+        [Display(Name = "Date Of Creation")]
         public DateTime DateOfCreation { get; set; }
     }
 }
