0d10d9e8cc909642ce5ff09c220e06df2e6004d0
MarwaHegazy1 <marwahegazy599@gmail.com>
2024-03-24 01:42:58 +0200

    EmployeeController - Edit and Details

--- ASP.NET Core MVC 03.PL/Controllers/EmployeeController.cs
+++ ASP.NET Core MVC 03.PL/Controllers/EmployeeController.cs
@@ -1,16 +1,21 @@
 ﻿using ASP.NET_Core_MVC.BLL.Interfaces;
 using ASP.NET_Core_MVC.DAL.Modules;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+using System;
 
 namespace ASP.NET_Core_MVC_03.PL.Controllers
 {
     public class EmployeeController : Controller
     {
         private readonly IEmployeeRepository _employeesRepo;
+        private readonly IWebHostEnvironment _env;
 
-        public EmployeeController(IEmployeeRepository employeesRepo)
+        public EmployeeController(IEmployeeRepository employeesRepo, IWebHostEnvironment env)
         {
             _employeesRepo = employeesRepo;
+            _env = env;
         }
         public IActionResult Index()
         {
@@ -34,6 +39,48 @@ namespace ASP.NET_Core_MVC_03.PL.Controllers
 
             }
             return View(employee);
+        }
+        public IActionResult Details(int? id, string viewName = "Details")
+        {
+            if (!id.HasValue)
+                return BadRequest(); // 400
+
+            var employee = _employeesRepo.Get(id.Value);
+
+            if (employee is null)
+                return NotFound(); // 404
+
+            return View(viewName, employee);
+        }
+        public IActionResult Edit(int? id)
+        {
+            return Details(id, "Edit");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit([FromRoute] int id, Employee employee)
+        {
+            if (id != employee.Id)
+                return BadRequest();
+
+            if (!ModelState.IsValid)
+                return View(employee);
+
+            try
+            {
+                _employeesRepo.Update(employee);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                if (_env.IsDevelopment())
+                    ModelState.AddModelError(string.Empty, ex.Message);
+                else
+                    ModelState.AddModelError(string.Empty, "An Error Has Occurred during Updating the Employee");
+
+                return View(employee);
+            }
         }
     }
 
