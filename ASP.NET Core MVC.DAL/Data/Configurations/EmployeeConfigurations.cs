eb3b74b0bc9378a8f5b70a4873afcf7620127311
MarwaHegazy1 <marwahegazy599@gmail.com>
2024-03-24 14:00:34 +0200

    Employee Controller Views (DisplayFor)

--- ASP.NET Core MVC.DAL/Data/Configurations/EmployeeConfigurations.cs
+++ ASP.NET Core MVC.DAL/Data/Configurations/EmployeeConfigurations.cs
@@ -19,7 +19,7 @@ namespace ASP.NET_Core_MVC.DAL.Data.Configurations
             builder.Property(E => E.Salary).HasColumnType("decimal(12, 2)");
             builder.Property(E => E.Gender).HasConversion(
                 (Gender) => Gender.ToString(),
-                (genderAsString) => (Gender)Enum.Parse(typeof(Gender), genderAsString, true));
+                (genderAsString) => (Gender) Enum.Parse(typeof(Gender), genderAsString, true));
         }
     }
 }
