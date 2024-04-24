41946aafd61192e224415dc574dcb3cac73312c7
MarwaHegazy1 <marwahegazy599@gmail.com>
2024-04-24 13:57:48 +0200

    Send Email

---
+++ ASP.NET Core MVC 03.PL/Services/EmailSender/IEmailSender.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace ASP.NET_Core_MVC_03.PL.Services.EmailSender
+{
+	public interface  IEmailSender
+	{
+		Task SendAsync(string from, string recipients, string subject, string body);
+	}
+}
